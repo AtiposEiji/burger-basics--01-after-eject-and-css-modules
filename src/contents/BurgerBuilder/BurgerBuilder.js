306d96c421afed0f9d9bc05f3bbb083b1d00f377
davide.brignoli@yourbiz.it <davide.brignoli@yourbiz.it>
2021-03-16 16:46:50 +0100

    finish checkout

--- src/contents/BurgerBuilder/BurgerBuilder.js
+++ src/contents/BurgerBuilder/BurgerBuilder.js
@@ -101,7 +101,8 @@ class BurgerBuilder extends Component {
                     show={this.state.purchasing}
                     modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary
-                        ingredients={this.state.ingredients} 
+                        ingredients={this.state.ingredients}
+                        price={this.state.totalPrice}
                         purchaseCanceled={this.purchaseCancelHandler}
                         purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
